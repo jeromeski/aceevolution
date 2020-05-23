b6342083bc527cca6018ce5245a8a86368d47022
Jeromeski <jeromegee@gmail.com>
2020-05-23 17:15:56 +0800

    AppBar Logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { useScrollTrigger} from '@material-ui/core';
-import {makeStyles } from '@material-ui/styles'
 
+import { useScrollTrigger } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -21,22 +21,27 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: '3em'
+
+  },
+  logo: {
+    height: '7em'
   }
 }));
 
-const Header = props => {
+const Header = () => {
   const classes = useStyles();
   return (
     <>
       <ElevationScroll>
         <AppBar position='fixed'>
-          <Toolbar>
-            <Typography variant='h3'>Ace Evolution</Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt='logo' className={classes.logo} />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
-      <div className={classes.toolbarMargin}/>
+      <div className={classes.toolbarMargin} />
     </>
   );
 };
