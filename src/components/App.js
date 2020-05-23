f4bd5ea50f117c96d1295517f399cb9f2edc8b98
Jeromeski <jeromegee@gmail.com>
2020-05-23 13:57:56 +0800

    Styling - Themes setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './ui/Header';
-
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './ui/Theme';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import { useScrollTrigger } from '@material-ui/core';
 
-const Header = (props) => {
+function ElevationScroll(props) {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  });
+}
+
+const Header = props => {
   return (
-    <AppBar position='fixed'>
-      <Toolbar>
-        Ace Evolution
-      </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position='fixed'>
+        <Toolbar>Ace Evolution</Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 };
 
