22e447443c2af1e83aa772bf3044bbecebdf0d62
Jeromeski <jeromegee@gmail.com>
2020-05-23 16:14:57 +0800

    Styling - Inline

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { useScrollTrigger, Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { useScrollTrigger} from '@material-ui/core';
+import {makeStyles } from '@material-ui/styles'
+
 
 function ElevationScroll(props) {
-  const { children, window } = props;
+  const { children } = props;
 
   const trigger = useScrollTrigger({
     disableHysteresis: true,
@@ -16,15 +19,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}));
+
 const Header = props => {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h3'>Ace Evolution</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography variant='h3'>Ace Evolution</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin}/>
+    </>
   );
 };
 
