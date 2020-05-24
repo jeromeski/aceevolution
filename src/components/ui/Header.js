23d9d757e8c5368f468f031ad61a67955dd34448
Jeromeski <jeromegee@gmail.com>
2020-05-24 11:14:03 +0800

    Using Menus - Selection

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -71,6 +71,7 @@ const Header = (props) => {
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0)
 
   const handleChange = (e, value) => {
     setValue(value);
@@ -81,10 +82,20 @@ const Header = (props) => {
     setOpen(true);
   };
 
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(i)
+  }
+
   const handleClose = e => {
     setAnchorEl(null);
     setOpen(false);
   };
+
+  const menuOptions = [{name: 'Services', link: '/services'}, {name: 'Custom Software Development', link: '/customsoftware'}, {name: 'Mobile App Development', link: '/mobileapps'}, {name: 'Website Development', link: '/websites'}]
+
+  
 
   useEffect(() => {
     if (window.location.pathname === '/' && value !== 0) {
@@ -170,16 +181,16 @@ const Header = (props) => {
               classes={{paper: classes.menu}}
               elevation={0}
               >
-              <MenuItem onClick={() => {handleClose(); setValue(1) }} component={Link} to='/services'
-              classes={{root: classes.menuItem}}
-              >
-                Services
-              </MenuItem>
-              <MenuItem onClick={() => {handleClose(); setValue(1) }} component={Link} to='/customsoftware' classes={{root: classes.menuItem}}>
-                Custom Software Development
-              </MenuItem>
-              <MenuItem onClick={() => {handleClose(); setValue(1) }} component={Link} to='/mobileapps' classes={{root: classes.menuItem}}>Mobile App Development</MenuItem>
-              <MenuItem onClick={() => {handleClose(); setValue(1) }} component={Link} to='/websites' classes={{root: classes.menuItem}}>Website Development</MenuItem>
+              {menuOptions.map((option, i) => (
+                <MenuItem
+                  key={option}
+                  component={Link}
+                  to={option.link}
+                  classes={{root: classes.menuItem}}
+                  onClick={(event)=>{handleMenuItemClick(event, i); setValue(1); handleClose()}}
+                  selected={i === selectedIndex && value === 1}
+                >{option.name}</MenuItem>
+              ))}
             </Menu>
           </Toolbar>
         </AppBar>
