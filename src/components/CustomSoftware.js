d2e5dd5fc2ca89e03d74b00b20826e37e14fcb10
Jeromeski <jeromegee@gmail.com>
2020-06-07 08:13:45 +0800

    Custome Software Animation Row Pt. 1

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -8,6 +8,8 @@ import forwardArrow from '../assets/forwardArrow.svg';
 import lightbulb from '../assets/bulb.svg';
 import stopwatch from '../assets/stopwatch.svg';
 import cash from '../assets/cash.svg';
+import documentsAnimation from '../animations/documentsAnimation/data';
+import scaleAnimation from '../animations/scaleAnimation/data.json';
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -21,12 +23,36 @@ const useStyles = makeStyles(theme => ({
     paddingRight: '5em',
     paddingTop: '2em',
     paddingBottom: '10em'
+  },
+  itemContainer: {
+    maxWidth: '40em'
   }
 }));
+
+const documentsOptions = {
+  loop: true,
+  autoplay: false,
+  animationData: documentsAnimation,
+  renderSettings: {
+    preserveAspectRation: 'xMidYMid slice'
+  }
+};
+
+const scaleOptions = {
+  loop: true,
+  autoplay: false,
+  animationData: scaleAnimation,
+  renderSettings: {
+    preserveAspectRation: 'xMidYMid slice'
+  }
+};
 
 const CustomSoftware = props => {
   const classes = useStyles();
   const theme = useTheme();
+
+  
+
   return (
     <Grid container direction='column' className={classes.mainContainer}>
       <Grid item container direction='row'>
@@ -36,10 +62,9 @@ const CustomSoftware = props => {
           style={{ marginRight: '1em', marginLeft: '-3.5em' }}>
           <IconButton
             component={Link}
-            style={{ background: 'transparent' }}
+            style={{ backgroundColor: 'transparent' }}
             to='/services'
             onClick={() => {
-              props.setValue(1);
               props.setSelectedIndex(0);
             }}>
             <img src={backArrow} alt='Back to services page' />
@@ -79,7 +104,6 @@ const CustomSoftware = props => {
             component={Link}
             to='/mobileapps'
             onClick={() => {
-              props.setValue(1);
               props.setSelectedIndex(2);
             }}>
             <img
@@ -135,6 +159,60 @@ const CustomSoftware = props => {
           </Grid>
           <Grid item>
             <img src={cash} alt='cash' />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction='row'>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4'>Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+
+              <Typography variant='body1' paragraph>
+                Billions are spent annually on the purchasing, printing, and
+                distribution of paper. On top of the massive environmental
+                impact this has, it causes harm to your bottom line as well.
+              </Typography>
+
+              <Typography variant='body1' paragraph>
+                By utilizing digital forms and documents you can remove these
+                obsolete expenses, accelerate your communication, and help the
+                Earth.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              isStopped={true}
+              style={{ maxHeight: 325, maxWidth: 275 }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              isStopped={true}
+              style={{ maxHeight: 260, maxWidth: 280 }}
+            />
+          </Grid>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4' align='right'>Scale</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' align='right' paragraph>
+                Whether you’re a large brand, just getting started, or taking
+                off right now, our application architecture ensures pain-free
+                growth and reliability.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
