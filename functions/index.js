0200dbecbe485d70bf2fc78e8c7c082adbe5d678
Jeromeski <jeromegee@gmail.com>
2020-06-11 05:58:50 +0800

    Nodemailer Fix

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,37 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
-const admin = require('firebase-admin')
-const config = functions.config()
+const admin = require('firebase-admin');
+const config = functions.config();
+const cors = require('cors')({origin: true})
 
-admin.initializeApp()
+admin.initializeApp();
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', auth: {
-    user: config.user.email, pass: config.user.password
+  service: 'Gmail',
+  auth: {
+    user: config.user.email,
+    pass: config.user.password
   }
-})
+});
 
-let mailOptions = {from: 'Arc Development', to: 'jerprojone@gmail.com', subject: 'Testing nodemailer', text: 'Test successful' }
+let mailOptions = {
+  from: 'Arc Development',
+  to: 'jerprojone@gmail.com',
+  subject: 'Testing nodemailer',
+  text: 'Test successful'
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.sendMail = functions.https.onRequest((request, response) => {
- transporter.sendMail(mailOptions,error => {
-   if (error) {
-     response.send(error)
-   } else {
-     response.send('Message sent successfully');
-   }
- });
+  cors(request, response, () => {
+    transporter.sendMail(mailOptions, error => {
+      if (error) {
+        response.send(error);
+      } else {
+        response.send('Message sent successfully');
+      }
+    });
+  })
 });
